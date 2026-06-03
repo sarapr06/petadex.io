acd5aeea40a1fa94c965454ed781970c1df4da40
Sara Parvaresh Rizi <sara_parvaresh@yahoo.com>
2026-06-03 16:50:42 -0400

    Fix CSS HMR error by using static stylesheet imports. Move Mol* and feature-viewer CSS to top-level imports to avoid mini-css-extract-plugin removeChild failures on hot reload.

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ThemeProvider } from './src/context/ThemeContext'
 import Layout from './src/components/layout'
 import './src/styles/global.css';
+import 'molstar/lib/mol-plugin-ui/skin/light.scss';
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
